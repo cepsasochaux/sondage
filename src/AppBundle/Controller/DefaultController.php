a193f2f1a46960ca4db9716a0311503905bf7080
Léo <Lo>
2017-05-06 16:20:51 +0200

    last

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,6 +13,22 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $host_name  = "db680319702.db.1and1.com";
+        $database   = "db680319702";
+        $user_name  = "dbo680319702";
+        $password   = "cepsa1234";
+
+
+        $connect = mysqli_connect($host_name, $user_name, $password, $database);
+
+        if(mysqli_connect_errno())
+        {
+            var_dump('La connexion au serveur MySQL a échoué: '.mysqli_connect_error());
+        }
+        else
+        {
+            dump('Connexion au serveur MySQL établie avec succès');
+        }
         // replace this example code with whatever you need
         return $this->render('default/myindex.html.twig');
     }
