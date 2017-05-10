4cafc6a365699f35a1af65acbcd87d0c45730a3c
118450 <u89185910@cbi-bs1.server.lan>
2017-05-10 14:58:10 +0200

    BDD

---
+++ src/AppBundle/Entity/Client.php
@@ -0,0 +1,283 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Client
+ *
+ * @ORM\Table(name="client")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ClientRepository")
+ */
+class Client
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="code", type="string", length=255)
+     */
+    private $code;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="status", type="integer")
+     */
+    private $status;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="sexe", type="integer")
+     */
+    private $sexe;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="age", type="integer")
+     */
+    private $age;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="profession", type="string", length=255)
+     */
+    private $profession;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="enfant", type="integer")
+     */
+    private $enfant;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="enfant_age", type="integer")
+     */
+    private $enfantAge;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="situation", type="integer")
+     */
+    private $situation;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     *
+     * @return Client
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set status
+     *
+     * @param integer $status
+     *
+     * @return Client
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return int
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set sexe
+     *
+     * @param integer $sexe
+     *
+     * @return Client
+     */
+    public function setSexe($sexe)
+    {
+        $this->sexe = $sexe;
+
+        return $this;
+    }
+
+    /**
+     * Get sexe
+     *
+     * @return int
+     */
+    public function getSexe()
+    {
+        return $this->sexe;
+    }
+
+    /**
+     * Set age
+     *
+     * @param integer $age
+     *
+     * @return Client
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    /**
+     * Get age
+     *
+     * @return int
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * Set profession
+     *
+     * @param string $profession
+     *
+     * @return Client
+     */
+    public function setProfession($profession)
+    {
+        $this->profession = $profession;
+
+        return $this;
+    }
+
+    /**
+     * Get profession
+     *
+     * @return string
+     */
+    public function getProfession()
+    {
+        return $this->profession;
+    }
+
+    /**
+     * Set enfant
+     *
+     * @param integer $enfant
+     *
+     * @return Client
+     */
+    public function setEnfant($enfant)
+    {
+        $this->enfant = $enfant;
+
+        return $this;
+    }
+
+    /**
+     * Get enfant
+     *
+     * @return int
+     */
+    public function getEnfant()
+    {
+        return $this->enfant;
+    }
+
+    /**
+     * Set enfantAge
+     *
+     * @param integer $enfantAge
+     *
+     * @return Client
+     */
+    public function setEnfantAge($enfantAge)
+    {
+        $this->enfantAge = $enfantAge;
+
+        return $this;
+    }
+
+    /**
+     * Get enfantAge
+     *
+     * @return int
+     */
+    public function getEnfantAge()
+    {
+        return $this->enfantAge;
+    }
+
+    /**
+     * Set situation
+     *
+     * @param integer $situation
+     *
+     * @return Client
+     */
+    public function setSituation($situation)
+    {
+        $this->situation = $situation;
+
+        return $this;
+    }
+
+    /**
+     * Get situation
+     *
+     * @return int
+     */
+    public function getSituation()
+    {
+        return $this->situation;
+    }
+}
+
