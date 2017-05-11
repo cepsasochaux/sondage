1a94336bb3eba5806cb06bb9d5bfc3ae0bcc73e2
118450 <u89185910@cbi-bs1.server.lan>
2017-05-11 13:24:31 +0200

    bdd

---
+++ src/AppBundle/Entity/Reponse.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Reponse
+ *
+ * @ORM\Table(name="reponse")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ReponseRepository")
+ */
+class Reponse
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="question_id", type="integer")
+     */
+    private $questionId;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="client_id", type="integer")
+     */
+    private $clientId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="value", type="string", length=255)
+     */
+    private $value;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="more", type="string", length=255)
+     */
+    private $more;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set questionId
+     *
+     * @param integer $questionId
+     *
+     * @return Reponse
+     */
+    public function setQuestionId($questionId)
+    {
+        $this->questionId = $questionId;
+
+        return $this;
+    }
+
+    /**
+     * Get questionId
+     *
+     * @return int
+     */
+    public function getQuestionId()
+    {
+        return $this->questionId;
+    }
+
+    /**
+     * Set clientId
+     *
+     * @param integer $clientId
+     *
+     * @return Reponse
+     */
+    public function setClientId($clientId)
+    {
+        $this->clientId = $clientId;
+
+        return $this;
+    }
+
+    /**
+     * Get clientId
+     *
+     * @return int
+     */
+    public function getClientId()
+    {
+        return $this->clientId;
+    }
+
+    /**
+     * Set value
+     *
+     * @param string $value
+     *
+     * @return Reponse
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Set more
+     *
+     * @param string $more
+     *
+     * @return Reponse
+     */
+    public function setMore($more)
+    {
+        $this->more = $more;
+
+        return $this;
+    }
+
+    /**
+     * Get more
+     *
+     * @return string
+     */
+    public function getMore()
+    {
+        return $this->more;
+    }
+}
+
