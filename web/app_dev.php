121726eda2ef987bd714a65f71552f2a6ac0ab00
Léo <Lo>
2017-05-15 14:11:11 +0200

    banane

--- web/app_dev.php
+++ web/app_dev.php
@@ -13,7 +13,7 @@ use Symfony\Component\Debug\Debug;
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !(in_array(@$_SERVER['REMOTE_ADDR'], ['62.39.213.48', '::1']) || php_sapi_name() === 'cli-server')
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['217.108.60.193', '::1']) || php_sapi_name() === 'cli-server')
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['62.39.213.48', '::1'], ['217.108.60.193', '::1']) || php_sapi_name() === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
