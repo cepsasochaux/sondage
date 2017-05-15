18d1b2f39ac321c51990e1f381b73580e2cadd32
Léo <Lo>
2017-05-15 11:19:07 +0200

    banane

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -150,9 +150,34 @@ class DefaultController extends Controller
 
         if ($form->isSubmitted() && $form->isValid()) {
             $client = $form->getData();
-            $message = "Blabla";
-            mail('leo.meyer12@gmail.com', 'Tombola ticket virtuel', $message, null,
-                '-fcontact@cepsa-sochaux-sondage.com');
+            $message = '<html><body>';
+            $message .= '<img src="//css-tricks.com/examples/WebsiteChangeRequestForm/images/wcrf-header.png" alt="Website Change Request" />';
+            $message .= '<table rules="all" style="border-color: #666;" cellpadding="10">';
+            $message .= "<tr style='background: #eee;'><td><strong>Name:</strong> </td><td>" . strip_tags($_POST['req-name']) . "</td></tr>";
+            $message .= "<tr><td><strong>Email:</strong> </td><td>" . strip_tags($_POST['req-email']) . "</td></tr>";
+            $message .= "<tr><td><strong>Type of Change:</strong> </td><td>" . strip_tags($_POST['typeOfChange']) . "</td></tr>";
+            $message .= "<tr><td><strong>Urgency:</strong> </td><td>" . strip_tags($_POST['urgency']) . "</td></tr>";
+            $message .= "<tr><td><strong>URL To Change (main):</strong> </td><td>" . $_POST['URL-main'] . "</td></tr>";
+            $addURLS = $_POST['addURLS'];
+            if (($addURLS) != '') {
+                $message .= "<tr><td><strong>URL To Change (additional):</strong> </td><td>" . strip_tags($addURLS) . "</td></tr>";
+            }
+            $curText = htmlentities($_POST['curText']);
+            if (($curText) != '') {
+                $message .= "<tr><td><strong>CURRENT Content:</strong> </td><td>" . $curText . "</td></tr>";
+            }
+            $message .= "<tr><td><strong>NEW Content:</strong> </td><td>" . htmlentities($_POST['newText']) . "</td></tr>";
+            $message .= "</table>";
+            $message .= "</body></html>";
+            $to = 'leo.meyer12@gmail.com';
+
+            $subject = 'Tombola ticket virtuel';
+
+            $headers = "From: " . strip_tags($_POST['req-email']) . "\r\n";
+            $headers .= "Reply-To: ". strip_tags($_POST['req-email']) . "\r\n";
+            $headers .= "MIME-Version: 1.0\r\n";
+            $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+            mail($to, $subject, $message, $headers);
             die;
 
         }
